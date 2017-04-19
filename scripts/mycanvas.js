2a2d06906b7d3bf5e5ea8fd1d40fdc0d4dde0fa2
Damir Kal (Laptop) <damirkalender@hotmail.com>
2017-04-19 22:57:57 +0200

    state refactoring

--- scripts/mycanvas.js
+++ scripts/mycanvas.js
@@ -1,9 +1,195 @@
 
 jQuery(document).ready(function ($) {
+    var nodes = []
+    var transitions = []
+    var edges = []
+    var selected;
+    var edgePending = null;
 
     class Place {
         constructor(x, y, radius, text, tokens) {
             this.drawObject = createNode(x, y, radius, text, tokens)
+        }
+    }
+
+    class Istate {
+        constructor() { }
+        canvasClick() { }
+        canvasDoubleClick() { }
+        placeClick() { }
+        transitionClick() { }
+        AddnodeClick() { }
+        AddEdgeClick() { }
+        SelectedButtonUpdate() { }
+        keydownEvent() { }
+    }
+
+
+    class DefaultState extends Istate {
+        constructor() { super(); }
+
+        canvasClick(node, event) {
+            //clicked on empty spot -> deselect
+            deselect();
+        }
+        canvasDoubleClick() { }
+        placeClick(node, event) {
+            console.log(' clicked on ');
+            console.log(node.name);
+            console.log(event);
+
+            //set current place as selected
+            selected.children[0].text = "Selected: " + node.name;
+            selected.current = node;
+            selected.redraw();
+            event.stopPropagation();
+            state.currentState = state.selectionState;
+        }
+
+        transitionClick(node, event) {
+            selected.children[0].text = "Selected: " + node.name;
+            selected.current = node;
+            selected.redraw();
+            event.stopPropagation();
+            state.currentState = state.selectionState;
+        }
+        AddnodeClick() { }
+        AddEdgeClick() {
+            console.log("select node first!")
+        }
+        keydownEvent(event) {
+
+            if (event.which == 84)
+                nodes.push(createTransition(300, 300, 100, "cheeki breeki"));
+        }
+    }
+
+    class SelectionState extends Istate {
+        constructor() { super(); }
+        canvasDoubleClick() { }
+        canvasClick() {
+            deselect();
+            state.currentState = state.defaultState;
+            //console.log(state);
+        }
+
+        placeClick(node, event) {
+            //    console.log(' del event: ')
+            // console.log(event)
+
+            //switch to selected
+            selected.children[0].text = "Selected: " + node.name;
+            selected.current = node;
+            selected.redraw();
+            event.stopPropagation();
+        }
+
+        transitionClick(node, event) {
+            selected.children[0].text = "Selected: " + node.name;
+            selected.current = node;
+            selected.redraw();
+
+
+            event.stopPropagation();
+        }
+        AddnodeClick() { }
+        AddEdgeClick() {
+
+            var line = canvas.display.line({
+                start: { x: selected.current.x, y: selected.current.y },
+                stroke: "11px #0aa",
+                cap: "round"
+            }).add();
+
+            edgePending = line;
+
+            //switch state
+            state.currentState = state.edgePendingState;
+
+        }
+        keydownEvent() { }
+    }
+
+    class ExecutionState extends Istate {
+        constructor() { super(); }
+        canvasClick() { }
+        placeClick() { }
+        transitionClick() { }
+        AddnodeClick() { }
+        AddEdgeClick() { }
+        keydownEvent() { }
+    }
+
+    function edgePlacementValidation(node) {
+        if (selected.current === node) {
+            console.log('cant point to self!')
+            return false;
+        }
+        if (selected.current.nodeType == node.nodeType) {
+            console.log("same type!")
+            console.log(selected.current);
+            console.log(node);
+            return false;
+        }
+
+        var mapped = node.incomingEdges.map(function (item) { return item.From; });
+
+        if ($.inArray(selected.current, mapped) != -1) {
+            console.log("edge already exists");
+            return false;
+        }
+
+        return true;
+    }
+    class EdgePendingState extends Istate {
+        constructor() { super(); }
+        canvasClick() {
+            console.log("click on an object")
+        }
+
+        placeClick(node, event) {
+
+            if (!edgePlacementValidation(node)) return;
+
+            var newEdge = createEdge(selected.current, node);
+            selected.current = null;
+            edgePending.remove();
+            edgePending = null;
+
+            //switch states
+            state.currentState = state.defaultState;
+        }
+
+        transitionClick(node, event) {
+            if (!edgePlacementValidation(node)) return;
+
+            var newEdge = createEdge(selected.current, node);
+            selected.current = null;
+            edgePending.remove();
+            edgePending = null;
+
+            //switch states
+            state.currentState = state.defaultState;
+
+        }
+
+        AddnodeClick() { }
+        AddEdgeClick() { }
+        keydownEvent(event) {
+            edgePending.remove();
+            edgePending = null;
+
+            state.currentState = state.selectionState;
+        }
+    }
+
+    class State {
+        constructor() {
+            this.defaultState = new DefaultState();
+            this.selectionState = new SelectionState();
+            this.executionState = new ExecutionState();
+            this.edgePendingState = new EdgePendingState();
+            this.currentState = this.defaultState;
         }
     }
 
@@ -22,18 +208,13 @@ jQuery(document).ready(function ($) {
         fps: 60,
     });
 
+    var state = new State();
+
     $canvas.contextmenu(function () {
         return false;
     });
 
-    canvas.bind("click tap", function (event) {
-        if (!edgePending) {
-            selected.current = null;
-            selected.children[0].text = "None selected";
-            selected.redraw();
-        }
-        console.log(event)
-    });
+    canvas.bind("click tap", function (event) { state.currentState.canvasClick(event); });
 
     canvas.bind("mousemove", function () {
         if (edgePending) {
@@ -42,93 +223,20 @@ jQuery(document).ready(function ($) {
         }
     });
 
-    canvas.bind("keydown", function (event) {
-        if (edgePending) {
-            edgePending.remove();
-            edgePending = null;
-        }
-    })
+    canvas.bind("keydown", function (event) { state.currentState.keydownEvent(event); })
 
-    var nodes = []
-    var transitions = []
-    var edges = []
-    var selected;
-    var edgePending = null;
 
     initMenu();
-    //var node = createNode(400, 400, 50, "P1", 1)
-    //var node2 = createNode(600, 200, 50, "P2", 0)
-    //  var edge = createEdge(node, node2);
-    //var t1 = createTransition(700, 400, 75, "T1")
-    //nodes.push(node, node2);
-    //edges.push(edge);
 
     //pseudo interface
-    class Istate {
-        constructor() { }
-        canvasClick() { }
-        canvasDoubleClick() { }
-        placeClick() { }
-        transitionClick() { }
-        AddnodeClick() { }
-        AddEdgeClick() { }
-        SelectedButtonUpdate() { }
+    function deselect() {
+        selected.current = null;
+        selected.children[0].text = "None selected";
+        selected.redraw();
+        console.log("currently selected: ");
+        console.log(selected.current)
     }
 
-    class DefaultState extends Istate {
-        constructor() { super(); }
-        canvasClick() { }
-        canvasDoubleClick() { }
-        placeClick() { }
-        transitionClick() { }
-        AddnodeClick() { }
-        AddEdgeClick() { }
-        SelectedButtonUpdate() { }
-    }
-
-    class SelectionState extends Istate {
-        constructor() { super(); }
-        canvasDoubleClick() { }
-        canvasClick() { }
-        placeClick() { }
-        transitionClick() { }
-        AddnodeClick() { }
-        AddEdgeClick() { }
-        SelectedButtonUpdate() { }
-    }
-
-    class ExecutionState extends Istate {
-        constructor() { super(); }
-        canvasClick() { }
-        placeClick() { }
-        transitionClick() { }
-        AddnodeClick() { }
-        AddEdgeClick() { }
-        SelectedButtonUpdate() { }
-    }
-    class EdgePendingState extends Istate {
-        constructor() { super(); }
-        canvasClick() { }
-        placeClick() { }
-        transitionClick() { }
-        AddnodeClick() { }
-        AddEdgeClick() { }
-        SelectedButtonUpdate() { }
-    }
-
-    class State {
-        constructor() {
-            var defaultState = new DefaultState();
-            var selectedState = new SelectionState();
-            var executionState = new ExecutionState();
-            var edgePendingState = new EdgePendingState();
-            this.currentState = defaultState;
-        }
-    }
-
-    var node = new Place(400, 400, 50, "P1", 1);
-    var state = new State();
-    console.log(state)
 
     function createEdge(nodeA, nodeB) {
         var line = canvas.display.line({
@@ -144,11 +252,6 @@ jQuery(document).ready(function ($) {
         line.To = nodeB;
         line.redraw();
 
-        line.bind("click", function (event) {
-            console.log('clicked line');
-            console.log(this);
-        });
-
         var triangle = canvas.display.polygon({
             x: 0,
             y: 0,
@@ -157,18 +260,19 @@ jQuery(document).ready(function ($) {
             rotation: 0,
             fill: "#0da"
         });
-
         line.addChild(triangle);
 
-        lineOnEdge(node);
+        lineOnEdge(nodeA);
         lineOnEdge(nodeB);
         canvas.addChild(line);
         line.zIndex = 0;
+        triangle.zIndex = 3;    //doesnt work?
+
     }
 
     function initMenu() {
         var addNode = createButton(10, 10, 100, 50, "Add node");
-        addNode.bind("click tap", function () { nodes.push(createNode(canvas.width / 2, canvas.height / 2, 50, "new node", 0)) });
+        addNode.bind("click tap", function () { nodes.push(createNode(canvas.width / 2, canvas.height / 2, 50, "new node", 1)) });
 
         addNode.bind("mouseenter", function (event) { this.fill = "orange"; this.redraw() });
         addNode.bind("mouseleave", function (event) { this.fill = "black"; this.redraw() });
@@ -182,15 +286,8 @@ jQuery(document).ready(function ($) {
         addEdge.bind("mouseup", function (event) { this.fill = "orange"; this.redraw() });
 
         addEdge.bind("click", function (event) {
-            if (selected.current) {
-                var line = canvas.display.line({
-                    start: { x: selected.current.x, y: selected.current.y },
-                    stroke: "11px #0aa",
-                    cap: "round"
-                }).add();
-
-                edgePending = line;
-            }
+            state.currentState.AddEdgeClick();
+            event.stopPropagation();
         })
 
         selected = createButton(230, 10, 100, 50, "None selected");
@@ -201,6 +298,7 @@ jQuery(document).ready(function ($) {
         canvas.setLoop(function () {
             coords.children[0].text = "X: " + canvas.mouse.x + " \\ " + "Y: " + canvas.mouse.y;;
 
+            //TODO STATE
             if (selected.current) {
                 var offset = 0;
                 if (selected.current.nodeType == "transition") offset = selected.current.width / 2;
@@ -222,7 +320,6 @@ jQuery(document).ready(function ($) {
             fill: "#000"
         }).add();
 
-
         var buttonText = canvas.display.text({
             x: button.width / 2,
             y: button.height / 2,
@@ -241,31 +338,77 @@ jQuery(document).ready(function ($) {
         var adjEdges = node.outgoingEdges.concat(node.incomingEdges);
 
         adjEdges.forEach(function (currentEdge) {
+
+            var rectWidthFrom = currentEdge.From.width / 2;
+            var rectWidthTo = currentEdge.To.width / 2;
+
+            var subtractionRadius = Math.max(rectWidthFrom, rectWidthTo);
+
+            var v1x = currentEdge.start.x = currentEdge.From.x + rectWidthFrom;
+            var v1y = currentEdge.start.y = currentEdge.From.y + rectWidthFrom;
+            var v2x = currentEdge.end.x = currentEdge.To.x + rectWidthTo;
+            var v2y = currentEdge.end.y = currentEdge.To.y + rectWidthTo;
+
             var triangle = currentEdge.children[0];
 
-            var vx = currentEdge.From.x - currentEdge.To.x;
-            var vy = currentEdge.From.y - currentEdge.To.y;
-            var vector = new Victor(vx, vy);
-            var subVector = vector.clone().normalize().multiply(new Victor(node.radius, node.radius));
+            var vector = new Victor(v2x - v1x, v2y - v1y);
 
-            currentEdge.start.x = currentEdge.From.x - subVector.x;
-            currentEdge.start.y = currentEdge.From.y - subVector.y;
+            var subvec = vector.clone().normalize().multiply(new Victor(subtractionRadius, subtractionRadius));
 
-            var sub2 = vector.clone().normalize().multiply(new Victor(triangle.radius, triangle.radius));
-            currentEdge.end.x = currentEdge.To.x + subVector.x + sub2.x;
-            currentEdge.end.y = currentEdge.To.y + subVector.y + sub2.y;
+            currentEdge.start.x += subvec.x;
+            currentEdge.start.y += subvec.y;
+            currentEdge.end.x -= subvec.x;
+            currentEdge.end.y -= subvec.y;
 
-            var v2 = new Victor(vy, vx);
-            triangle.rotation = 270 - v2.angleDeg();
+
+
+
+            // var offsetFromx = 0;
+            // var offsetFromy = 0;
+            // var offsetTox = 0;
+            // var offsetToy = 0;
+
+            // if (currentEdge.From.nodeType == "transition") {
+            //     console.log('ADDING OFFSET1')
+            //     offsetFromx = currentEdge.From.width / 2;
+            //     offsetFromy = currentEdge.From.width / 2;
+            //     console.log(offsetFromx);
+            // }
+            // if (currentEdge.To.nodeType == "transition") {
+            //     console.log('ADDING OFFSET2')
+            //     offsetTox = currentEdge.To.width / 2;
+            //     offsetToy = currentEdge.To.width / 2;
+            //     console.log();
+            // }
+
+            // var vx = (currentEdge.From.x - offsetFromx) - (currentEdge.To.x + offsetTox);
+            // var vy = (currentEdge.From.y - offsetFromy) - (currentEdge.To.y + offsetToy);
+
+            // //   console.log('fromtype: ' + currentEdge.From.nodeType)
+            // //   console.log('totype: ' + currentEdge.To.nodeType)
+
+            // var vector = new Victor(vx, vy);
+            // var subVector = vector.clone().normalize().multiply(new Victor(node.radius, node.radius));
+
+            // currentEdge.start.x = currentEdge.From.x - subVector.x;
+            // currentEdge.start.y = currentEdge.From.y - subVector.y;
+
+            // var sub2 = vector.clone().normalize().multiply(new Victor(triangle.radius, triangle.radius));
+            // currentEdge.end.x = currentEdge.To.x + subVector.x + sub2.x;
+            // currentEdge.end.y = currentEdge.To.y + subVector.y + sub2.y;
+
+            var v2 = new Victor(v2x - v1x, v2y - v1y);
+            triangle.rotation = v2.angleDeg();
 
             var vec = Victor(currentEdge.end.x - currentEdge.x, currentEdge.end.y - currentEdge.y);
-            var vec3 = new Victor(currentEdge.To.strokeWidth / 2, currentEdge.To.strokeWidth / 2)
+            var vec3 = new Victor(currentEdge.To.strokeWidth + triangle.radius / 2, currentEdge.To.strokeWidth + triangle.radius / 2);
             var subVec = vec.clone().normalize().multiply(vec3);
             vec.subtract(subVec);
 
             triangle.x = vec.x;
             triangle.y = vec.y;
-
+            currentEdge.end.x = triangle.abs_x;
+            currentEdge.end.y = triangle.abs_y;
         });
     }
 
@@ -287,41 +430,10 @@ jQuery(document).ready(function ($) {
             fill: "#0ba"
         });
 
-        transition.bind("click tap", function (event) {
-            if (edgePending) {
-                if (selected.current === transition) {
-                    console.log('cant point to self!')
-                    return;
-                }
-
-                var mapped = transition.incomingEdges.map(function (item) { return item.From; });
-
-                if ($.inArray(selected.current, mapped) != -1) {
-                    console.log("edge already exists");
-                    return;
-
-                }
-                var newEdge = createEdge(selected.current, transition);
-                selected.current = null;
-                edgePending.remove();
-                edgePending = null;
-
-
-            }
-
-            else {
-                selected.children[0].text = "Selected: " + this.name;
-                selected.current = this;
-                selected.redraw();
-                event.stopPropagation();
-            }
-
-            console.log(selected.current)
-        });
+        transition.bind("click tap", function (event) { state.currentState.transitionClick(this, event); });
 
         transition.bind("dblclick ", function (event) {
             //fire(this);
-
         });
 
         transition.dragAndDrop({
@@ -365,39 +477,8 @@ jQuery(document).ready(function ($) {
             fill: "#0ba"
         });
 
-        node.bind("click tap", function (event) {
-            if (edgePending) {
-
-                if (selected.current.nodeType == node.nodeType) return;
-
-                if (selected.current === node) {
-                    console.log('cant point to self!')
-                    return;
-                }
-
-                var mapped = node.incomingEdges.map(function (item) { return item.From; });
-
-                if ($.inArray(selected.current, mapped) != -1) {
-                    console.log("edge already exists");
-                    return;
-
-                }
-
-                var newEdge = createEdge(selected.current, node);
-                selected.current = null;
-                edgePending.remove();
-                edgePending = null;
-            }
-
-            else {
-                selected.children[0].text = "Selected: " + this.name;
-                selected.current = this;
-                selected.redraw();
-                event.stopPropagation();
-            }
-        });
-
-        node.bind("dblclick ", function (event) { fire(this); });
+        node.bind("click tap", function (event) { state.currentState.placeClick(node, event); });
+        node.bind("dblclick ", function (event) {            /*   fire(this);   */ });
 
         node.dragAndDrop({
             start: function () { },
